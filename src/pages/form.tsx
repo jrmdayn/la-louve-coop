17b79c4ec6816d45b4e935b41841e3ad618d1ef5
jrmdayn <dayanjeremie@gmail.com>
2020-06-14 09:00:45 +0200

    add form page

---
+++ src/pages/form.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const FormPage = () => (
+  <iframe
+    src="https://airtable.com/embed/shra2lHfoaSXTMVZC?backgroundColor=cyan"
+    frameBorder="0"
+    style={{ width: "100vw", height: "100vh" }}
+  ></iframe>
+)
+
+export default FormPage
